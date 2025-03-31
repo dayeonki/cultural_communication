0cb68270ad7926e7477cf76b12e260b4e5e62f50
Yu (Hope) Hou <houyu@usc.edu>
2025-03-31 16:53:27 -0400

    update ZH first batch

--- code/sample_zh_neologisms.py
+++ code/sample_zh_neologisms.py
@@ -12,9 +12,17 @@ with open('../data/language_situation_official_zh.jsonl', 'r') as f:
       
 random.seed(0)
 random.shuffle(total_neologisms)
-first_batch = total_neologisms[:BATCH_SIZE]
-print("\n".join([item['word'] for item in first_batch]))
+selected_first_batch = []
+for item in total_neologisms:
+    if item['word'] not in item['explanatory_hint']:
+        selected_first_batch.append(item)
+        print(item['word'])
+        # print(item['explanatory_hint'])
+        if len(selected_first_batch) == BATCH_SIZE:
+            break
+
+# print("\n".join([item['word'] for item in selected_first_batch]))
 
 with open('../data/zh_firstbatch.jsonl', 'w') as f:
-    for item in first_batch:
+    for item in selected_first_batch:
         f.write(json.dumps(item, ensure_ascii=False) + '\n')
